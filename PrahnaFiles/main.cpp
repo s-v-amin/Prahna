7ff3d26e177c87247e47191d3f8454cd685c9b58
s-v-amin <sahilaminbisak@gmail.com>
2023-06-29 11:59:47 -0400

    CMake + Testing Files

--- PrahnaFiles/main.cpp
+++ PrahnaFiles/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include "output.h"
 
-int main() {
-    
-    std::cout << "Hello, World!";
+void init(const string& message)
+{
+    cout << message << endl;
+}
+void slowPrint(const string& message)
+{
+    int size = message.size();
+
+    for(int i = 0; i < size; i++)
+    {
+        cout << message[i];
+        usleep(300000);
+        cout.flush();
+    }
+    cout << endl;
+}
+
+int main() 
+{
+    string message = "Initializing";
+    init(message);
+    slowPrint("Hello, World!");
     return 0;
 }
---
+++ PrahnaFiles/output.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "unistd.h"
+#include <string>
+using namespace std;
+
+void init(const string& message);
+void slowPrint(const string& message);
